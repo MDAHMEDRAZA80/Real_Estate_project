feaf8f00315a00b73eb68763841037c555a07d1f
MDAHMEDRAZA80 <ahmedraza8292@gmail.com>
2023-05-22 13:00:52 +0530

    index.js file Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Signup-Login/Login";
+import Signup from "./Components/Signup-Login/Signup";
+import BasicInfo from "./Components/AddProperties/BasicInfo";
+import PropertyDetails from "./Components/AddProperties/propertyDetails";
+import GeneralInfo from "./Components/AddProperties/generalinfo";
+import LocationInfo from "./Components/AddProperties/locationinfo";
+import Property from "./Components/property/Property";
+import Protected from "./Components/Protected/protected";
 
 function App() {
   return (
-   <>
-   </> 
+    <>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route
+          path="/"
+          element={
+            <Protected>
+              <Property />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/basicinfo"
+          element={
+            <Protected>
+              <BasicInfo />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/propertydeatils"
+          element={
+            <Protected>
+              <PropertyDetails />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/generalinfo"
+          element={
+            <Protected>
+              <GeneralInfo />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/locationinfo"
+          element={
+            <Protected>
+              <LocationInfo />
+            </Protected>
+          }
+        ></Route>
+      </Routes>
+    </>
   );
 }
 
